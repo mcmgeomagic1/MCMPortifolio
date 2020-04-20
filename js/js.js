cd6d9d43e909da45dc3c85b787c3600b84a46348
mcmgeomagic1 <59767266+mcmgeomagic1@users.noreply.github.com>
2020-04-19 23:03:24 -0700

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -17,3 +17,42 @@ $('.js--header-imagens').waypoint(function(direction) {
 $('.js--middle').waypoint(function(direction) {
 	$('.js--middle').addClass('animated fadeIn');
 }, {offset: '50%' });
+
+/* Scroll on buttons */
+$('.js--scroll-middle-container').click(function() {
+	$('html,body').animate({scrollTop: $('.js--middle-container').offset().top}, 1000)
+ });
+$('.js--scroll-to-portfolio').click(function() {
+	$('html,body').animate({scrollTop: $('.js--portfolio').offset().top},1000)
+});
+/* Smooth Navegation */
+    
+      $('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+   
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { 
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); 
+            $target.focus(); 
+          };
+        });
+      }
+    }
+   });
